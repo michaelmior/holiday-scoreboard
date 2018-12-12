e9d1a15c560de613bc9c6631f5405ec5cab44edb
Michael Mior <mmior@cs.rit.edu>
2018-12-12 17:45:33 -0500

    Add simple unstyled scoreboard

---
+++ src/Scoreboard.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import { restdb, realtimeURL } from './helper.js';
+
+class Scoreboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {ping: new Date(), evt: '', scores: []};
+
+    // connect to the realtime database stream
+    let eventSource = new EventSource(realtimeURL);
+
+    // check if the realtime connection is dead, reload client if dead
+    setInterval(() => {
+      let now = new Date().getTime();
+      let diff = (now - this.state.ping.getTime()) / 1000;
+
+      // haven't heard from the server in 20 secs?
+      if (diff > 20) {
+        // hard reload of client
+        window.location.reload();
+      }
+    }, 10000);
+
+    // listen on ping from server, keep time
+    eventSource.addEventListener('ping', function(e) {
+      this.setState(previousState => {
+        return {ping: new Date(e.data)};
+      });
+    }.bind(this), false);
+
+    // listen for database REST operations
+    eventSource.addEventListener('put', (e) => {
+      this.getScores()
+    }, false);
+
+    eventSource.addEventListener('delete', (e) => {
+      this.getScores()
+    }, false);
+
+    eventSource.addEventListener('post', (e) => {
+      this.getScores();
+    }, false);
+
+    eventSource.addEventListener("error", (e) => {
+      // typically lost network connection
+      console.log("Error", e);
+    });
+
+    eventSource.onmessage = (e) => {
+      // generic messages
+      console.log("Message", e);
+    }
+  }
+
+  getScores = () => {
+    restdb.get("/rest/scores")
+      .then(res => {
+        let scores = res.data;
+        
+        this.setState(previousState => {
+          return { scores };
+        });
+      });
+  }
+
+  componentDidMount() {
+    console.log("Start client");
+    this.getScores();
+  }
+
+  render() {
+    return this.state.scores.map(team =>
+      <div key={team._id}>{team.name} - {team.score}</div>
+    )
+  }
+}
+
+export default Scoreboard;
---
+++ src/helper.js
@@ -0,0 +1,17 @@
+// helper.js
+import axios from 'axios';
+
+// CORS enabled apikey
+const apikey = process.env.REACT_APP_RESTDB_KEY;
+
+// REST endpoint
+let restdb = axios.create({
+  baseURL: '/',
+  timeout: 1000,
+  headers: { 'x-apikey': apikey }
+});
+
+// Eventsource endpoint
+const realtimeURL = `https://scoreboard-ec2a.restdb.io/realtime?apikey=${apikey}`
+
+export {apikey, restdb, realtimeURL};
